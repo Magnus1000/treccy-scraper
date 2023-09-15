21715dbdd876d19bc7f00347f71caf45a29d7a44
Jack McDonald <jackmagnusmcdonald.com>
2023-09-15 17:05:33 -0400

    background

--- scripts/background.js
+++ scripts/background.js
@@ -3,6 +3,13 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'loadDrawer') {
       // Send a message to content script to load drawer
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        // Check if tabs array is empty or if the first tab is undefined
+        if (tabs.length === 0 || !tabs[0]) {
+          console.error('No active tab found');
+          return;
+        }
+  
+        // Proceed to send message to content script
         chrome.tabs.sendMessage(tabs[0].id, { action: 'loadDrawer' });
       });
     }
