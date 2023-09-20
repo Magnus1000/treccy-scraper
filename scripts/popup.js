c485ae5544760f9c2d3955bc22860f0bce9f692c
Jack McDonald <jackmagnusmcdonald.com>
2023-09-20 13:38:57 -0400

    Update popup js

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,22 +2,32 @@ console.log(`popup.js script loaded at ${Date.now()} ms`);
 
 // Initialize the popup checkbox state
 document.addEventListener('DOMContentLoaded', () => {
-const popupToggle = document.getElementById('popup-toggle-83a1371d7');
-
-// Fetch the stored checkbox state from chrome.storage
-chrome.runtime.sendMessage({ action: 'getCheckboxState' }, (response) => {
-  if (response) {
-    popupToggle.checked = true;
-  } else {
-    popupToggle.checked = false;
+  console.log("DOMContentLoaded event fired, script will proceed");
+  const popupToggle = document.getElementById('popup-toggle-83a1371d7');
+  
+  if (popupToggle === null) {
+    console.log("Error: Checkbox element not found");
+    return;
   }
-});
 
-// Add a click listener to the checkbox
-popupToggle.addEventListener('click', () => {
-  chrome.runtime.sendMessage({ action: 'storeCheckboxState', value: popupToggle.checked });
-  if (popupToggle.checked) {
-    chrome.runtime.sendMessage({ action: 'loadDrawer' });
-  }
-});
+  // Fetch the stored checkbox state from chrome.storage
+  chrome.runtime.sendMessage({ action: 'getCheckboxState' }, (response) => {
+    console.log("Received response for 'getCheckboxState'", response);
+    if (response) {
+      popupToggle.checked = true;
+    } else {
+      popupToggle.checked = false;
+    }
+  });
+
+  // Add a click listener to the checkbox
+  popupToggle.addEventListener('click', () => {
+    console.log("Checkbox clicked, sending message to store state and possibly load the drawer");
+    chrome.runtime.sendMessage({ action: 'storeCheckboxState', value: popupToggle.checked });
+
+    if (popupToggle.checked) {
+      console.log("Checkbox is checked, sending message to load the drawer");
+      chrome.runtime.sendMessage({ action: 'loadDrawer' });
+    }
+  });
 });
